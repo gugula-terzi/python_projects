067794efe915dd8ea4725ae42c635f341f23f059
gugula-terzi <kirill.terzi.ceiti@mail.ru>
2020-11-15 15:26:11 +0200

    added models

--- django/homework/home_page/models.py
+++ django/homework/home_page/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Lesson(models.Model):
+    
+    name = models.CharField(max_length=255, verbose_name='Название предмета')
+    slug = models.SlugField(unique=True)
+    
+
+    def __str__(self):
+        return self.name
+
+class Homework(models.Model):
+    
+    title = models.CharField(max_length=255, verbose_name='Название Д/З')
+    slug = models.SlugField(unique=True)
+    file = models.FileField(upload_to='files/')
+    description = models.TextField(verbose_name='Описание', null=True)
+    publication_date = models.DateField(default=date.today, verbose_name='Дата публикации Д/З')
+    expiration_date = models.DateField(verbose_name='Срок сдачи Д/З')
+    homework = models.ForeignKey(Lesson, verbose_name='Предмет', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
